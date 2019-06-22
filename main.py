e4a59f86a2ba1d13d40c6cdde1627cd14565b53d
Dobry_kot <dputilin@moysklad.ru>
2019-06-23 02:28:54 +0300

    delete double var

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ if __name__ == "__main__":
 
     username    = 'test'
     email       = 'test@example.com'
-    email = 'test@mail.ru'
     password    = 't4too7'
     first_name  = 'test'
     last_name   = 'test'
 
 # class for working with users 
-    slack = slack()
+    # slack = slack()
     # slack.user_invite(email       = email,
     #                   first_name  = first_name,
     #                   last_name   = last_name)
-    slack.user_inactive(username)
+    # slack.user_inactive(username)
